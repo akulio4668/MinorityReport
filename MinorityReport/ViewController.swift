4c155d40f6f1072806a68b688c2733f1b978d527
Akul Joshi <ajoshi@akuls-macbook-air.local>
2017-06-13 12:52:53 -0500

    This is a practice program on pan gestures

--- MinorityReport/ViewController.swift
+++ MinorityReport/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var futureLabel: UILabel!
+    @IBOutlet weak var preCogsLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func onDrag(_ sender: UIPanGestureRecognizer)
+    {
+        print("Drag")
+        
+        let point = sender.location(in: view)
+        print(point)
+        futureLabel.center = point
+        
+        if sender.state == .ended
+        {
+            print("End")
+            UIView.animate(withDuration: 0.75, animations:
+            {
+                self.futureLabel.center = self.view.center
+            })
+        }
     }
-
-
 }
 
